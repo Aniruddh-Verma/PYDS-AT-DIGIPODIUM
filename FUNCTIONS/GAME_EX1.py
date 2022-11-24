341745ac8f081238c6916e6a1f709c2ab02f1be3
Anirudh-Verma <eraniruddhverma2023@gmail.com>
2022-11-24 17:33:48 +0530

    update game_ex.py

--- FUNCTIONS/GAME_EX1.py
+++ FUNCTIONS/GAME_EX1.py
@@ -1,6 +1,6 @@
 import pgzrun
 
-HEIGHT = 300
+HEIGHT = 600
 WIDTH = 800
 
 p = Actor('ironman',(100,100))   ## 100 , 100 is the cordinates of the screen 
@@ -11,6 +11,12 @@ def draw():
       screen.fill('white')
       p.draw()
       c.draw()
-
-
+      #print("drawing")
+def update():
+      #print('updating')
+      p.x += 1
+      p.angle = -10
+      if p.x > WIDTH:
+            p.x = 0
+      print(p.x,p.y)
 pgzrun.go()
